377cc7a66afb605553c728c703fa3045743451d8
Vlad <vladsydorchuk.ln@gmail.com>
2024-10-16 22:21:43 +0300

    added responsive

--- js/main.js
+++ js/main.js
@@ -3,24 +3,18 @@ console.log(fruits);
 const productsTable = document.querySelector("#products__list");
 console.log(productsTable.innerHTML);
 
-fruits.forEach(x => {
-    var item = `
-        <div class="products__card">
-            <div class="product-img-container" style="background-image: url('img/fruits/${x.code}.jpg');">
+fruits
+    .sort((a, b) => a.name.localeCompare(b.name))
+    .forEach(x => {
+        var item = `
+            <div class="products__card">
+                <div class="product-img-container" style="background-image: url('img/fruits/${x.code}.jpg');">
                 </div>
-            <div class="product-body">
-                <div class="product-body-top">
+                <div class="product-body">
                     <p class="product-title">${x.name}</p>
-                    <p class="product-availiable ${x.available ? 'in-stock' : 'out-stock'}">${x.available ? 'В наявності' : 'Немає в наявності'}</p>
-                </div>
-                <div class="product-body-bottom">
-                    <p class="product-price">${x.price} ₴</p>
-                    <p class="product-price-info">Ціна за ${x.pricePer}</p>
-                    <p class="product-min-weight">мін. замовлення ${x.minOrder}</p>
                 </div>
             </div>
-        </div>
-    `;
+        `;
 
     productsTable.insertAdjacentHTML("beforeend", item);
 });
