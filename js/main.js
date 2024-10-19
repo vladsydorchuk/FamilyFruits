bc303e65d1457b43bce69e0b48b657848b643417
Vlad <vladsydorchuk.ln@gmail.com>
2024-10-19 09:25:19 +0300

    Added burger menu

--- js/main.js
+++ js/main.js
@@ -22,3 +22,24 @@ fruits
 
     productsTable.insertAdjacentHTML("beforeend", item);
 });
+
+// MENU
+
+let menuBtn = document.querySelector('.menu-btn');
+let menu = document.querySelector('.nav');
+let menuItem = document.querySelectorAll('.nav__link');
+
+menuBtn.addEventListener('click', function(){
+	menuBtn.classList.toggle('active');
+	menu.classList.toggle('active');
+})
+
+
+menuItem.forEach (function(menuItem) {
+  menuItem.addEventListener('click',function(){
+          menuBtn.classList.toggle('active');
+          menu.classList.toggle('active');
+  })
+})
+
+
