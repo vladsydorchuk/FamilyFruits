73ff7f80c1ae28f2787f8707c8cf69046aec8f9b
Vlad <vladsydorchuk.ln@gmail.com>
2024-10-23 00:01:10 +0300

    Added basket and product view click

--- js/main.js
+++ js/main.js
@@ -3,6 +3,26 @@ const productsTable = document.querySelector("#products__list");
 let sortedFruits = fruitsData.sort((a, b) => a.name.localeCompare(b.name));
 let sortedVegetables = vegetablesData.sort((a, b) => a.name.localeCompare(b.name));
 
+// let mainProductList = sortedFruits
+//     .concat(sortedVegetables)
+//     .filter(x => x.id == 2 || x.id == 5);
+
+// function OnLoadMainPage() {
+//     console.log(mainProductList);
+//     fillProductList(mainProductList);
+// }
+
+// OnLoadMainPage();
+
+
+
+// localStorage['selectedProducts'] = JSON.stringify(sortedFruits);
+
+// let d = JSON.parse(localStorage['selectedProducts']);
+
+// console.log(d);
+
+
 // MENU
 let menuBtn = document.querySelector('.menu-btn');
 let menu = document.querySelector('.nav');
@@ -14,12 +34,19 @@ let green = document.querySelector('#green-btn')
 let nuts = document.querySelector('#nuts-btn')
 let honey = document.querySelector('#honey-btn')
 
+let basketView = document.querySelector('.basket__container');
+let basketBtn = document.querySelector('#basketBtn')
+
+let productViewCard = document.querySelector('.product__view');
+
+fillProductList(sortedFruits, false);
+
 fruitsBtn.addEventListener('click', function(){
-    fillProductList(sortedFruits);
+    fillProductList(sortedFruits, false);
 });
 
 vegetables.addEventListener('click', function(){
-    fillProductList(sortedVegetables);
+    fillProductList(sortedVegetables, false);
 });
 
 green.addEventListener('click', function(){
@@ -39,6 +66,11 @@ menuBtn.addEventListener('click', function(){
 	menu.classList.toggle('active');
 })
 
+basketBtn.addEventListener('click', function() {
+    fillProductList([], true); // Clear list
+    basketView.classList.toggle('hide');
+});
+
 menuItem.forEach (function(menuItem) {
   menuItem.addEventListener('click',function(){
           menuBtn.classList.toggle('active');
@@ -48,13 +80,20 @@ menuItem.forEach (function(menuItem) {
 
 
 
-function fillProductList(items) {
+function fillProductList(items, hideList) {
+    if (hideList){
+        productsTable.classList.add('hide');
+    } else {
+        productsTable.classList.remove('hide');
+        basketView.classList.add('hide');
+    }
+    
     productsTable.innerHTML = '';
 
     items
         .forEach(x => {
             var item = `
-                <div class="products__card">
+                <div class="products__card" onclick="productClick()" productCategory="${x.category}" productId="${x.id}">
                     <div class="product-img-container" style="background-image: url('img/${x.category}/${x.code}.jpg');">
                     </div>
                     <div class="product-body">
@@ -69,4 +108,70 @@ function fillProductList(items) {
 
         productsTable.insertAdjacentHTML("beforeend", item);
     });
+}
+
+function productClick(){ 
+    let clickedElement = this.event.currentTarget;
+    let rootElement = clickedElement.closest('.products__card');
+
+    let productCategory = rootElement.getAttribute("productCategory");
+    let productId = rootElement.getAttribute("productId");
+
+    let x = null;
+    switch (productCategory) {
+        case 'fruits':
+            x = fruitsData.find(x => x.id == productId);
+            break;
+        case 'vegetables':
+            x = vegetablesData.find(x => x.id == productId);
+            break;
+    }
+
+    let itemHtml = `
+        <div class="product__view-container">
+            <div class="product__view-img" style="background-image: url('img/${x.category}/${x.code}.jpg');"></div>
+            <p class="product__view-title product-title">${x.name}</p>
+            <p class="product__view-min-weight">Мінімальне замовлення ${x.minOrder}</p>
+            <p class="product__view-available ${x.available ? "hide" : "red-text"}">
+                ${x.available ? "" : "Немає в наявності"}
+            </p>
+            <input ${x.available ? "" : "disabled"} class="product__view-weight-value" type="number" min="${x.minOrder.split(' ')[0]}" placeholder="Введіть кількість...">
+            <div class="product__view-buttons">
+                <button id="product__view-addBtn" 
+                    onclick="addProductToBasketBtn()" 
+                    style="visibility: ${x.available ? "visible" : "hidden"};">
+                Додати
+                </button>
+                <button id="product__view-closeBtn" onclick="closeProductViewPopup()">Закрити</button>
+            </div>
+        </div>
+    `;
+
+    productViewCard.innerHTML = '';
+    productViewCard.insertAdjacentHTML("beforeend", itemHtml);
+    productViewCard.classList.toggle('hide');
+}
+
+function addProductToBasketBtn() {
+    let clickedElement = this.event.currentTarget;
+    let rootElement = clickedElement.closest('.product__view-container');
+
+    let weight = rootElement.querySelector('.product__view-weight-value');
+    let minWeight = weight.getAttribute('min');
+
+    if (minWeight > weight.value) {
+        let minOrder = rootElement.querySelector('.product__view-min-weight');
+        minOrder.classList.add('red-text');
+
+        return;
+    }
+
+    console.log(weight.value >= minWeight ? 'ok' : '!ok');
+
+    console.log('product added');
+    productViewCard.classList.toggle('hide');
+}
+
+function closeProductViewPopup() {
+    productViewCard.classList.toggle('hide');
 }
